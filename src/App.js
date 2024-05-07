a3a7b3bc0189be047326e5c26ef9c56d6d6404f6
nirali <niralisanghani2001@gmail.com>
2024-05-07 16:19:50 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const[data,setData]=useState([]);
+  useEffect(()=>{
+    fetchAllStudents();
+  },[]);
+
+  const fetchAllStudents= async ()=>{
+    try{
+      const res=await axios.get("https://demo-react-app-8e694232037e.herokuapp.com/getstudents");
+      setData(res.data);
+    }catch(err){
+      console.log(err);
+    }
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <table>
+              <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Email</th>
+                <th>City</th>
+              </tr>
+         
+          {
+            data.map((item)=>
+              <tr>
+                  <td>{item.id}</td>
+                  <td>{item.fname}</td>
+                  <td>{item.lname}</td>
+                  <td>{item.gender}</td>
+                  <td>{item.email}</td>
+                  <td>{item.city}</td>
+              </tr>
+            )
+          }
+           </table>
       </header>
     </div>
   );
